94ca47991ec392ccbcd1964dde542bfd3c4457ed
Your Name <you@example.com>
2021-08-04 20:27:45 -0500

    Welcome server

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log ("Hola Mundo");
+// console.log ("Hola Mundo");
+/*
+Customer satisfaction API
+*/
+
+const express = require('express');
+const cors = require('cors');
+
+// Global app object
+const app = express();
+
+// Middleware config
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.get("/" function(req, res) {
+    res.send("Welcome to customer custom API!");
+
+});
+
+// Bootstrap server
+const server = app.listen(process.env.PORT || 3000, function () {
+    console.log(`Listening on port ${server.address().port}`);
+});
+
